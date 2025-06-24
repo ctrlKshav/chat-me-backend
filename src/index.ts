2d07dafcabebe49399e85ce276034c3284dea215
ctrlKshav <keshavm9978@gmail.com>
2025-06-24 16:15:29 +0530

    Test Database

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,22 @@
-import { Hono } from 'hono'
 
-const app = new Hono()
+import { PrismaD1 } from '@prisma/adapter-d1'
+import { PrismaClient } from '@prisma/client'
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+export interface Env {
+  DB: D1Database
+}
 
-export default app
+export default {
+  async fetch(
+    request: Request,
+    env: Env,
+    ctx: ExecutionContext
+  ): Promise<Response> {
+    const adapter = new PrismaD1(env.DB)
+    const prisma = new PrismaClient({ adapter })
+
+    const stores = await prisma.stores.findMany()
+    const result = JSON.stringify(stores)
+    return new Response(result)
+  },
+}
