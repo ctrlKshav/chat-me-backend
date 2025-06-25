be7f27d52d0b0ba40935e264dda72c8a51e2694a
ctrlKshav <keshavm9978@gmail.com>
2025-06-25 17:22:10 +0530

    Add Proxy Routes for addAnalytics and getSettings

--- src/routes/app_proxy/index.js
+++ src/routes/app_proxy/index.js
@@ -1,5 +1,5 @@
 import { Hono } from "hono";
-import clientProvider from "../../utils/clientProvider.js";
+import prisma from "../../utils/prisma";
 const proxyRouter = new Hono();
 
 /**
@@ -15,5 +15,72 @@ proxyRouter.get("/json", async (c) => {
     return c.json({ error: e }, 500);
   }
 });
+
+proxyRouter.get("/getSettings", async (c) => {
+
+  const myShop = c.req.query.shop
+  try {
+    if (!myShop) {
+      return c.json({ error: "No shop provided" });
+    }
+    const settings = await prisma.stores.findFirst({
+      where: {
+        OR: [
+          {
+            shop: myShop,
+          },
+          {
+            domain: myShop,
+          },
+        ],
+      },
+      select: {
+        whatsapp_number: true,
+        greeting_message: true,
+        whatsapp_message: true,
+      },
+    });
+    console.info(settings);
+    return c.json({ ...settings });
+  } catch (e) {
+    console.error(e);
+    return c.json({ error: e }, 500);
+  }
+});
+
+proxyRouter.post("/addAnalytics", async (c) => {
+  try {
+    const data = await c.req.parseBody();
+    const { country, device_type, path, shopUrl, platform, clicked } = data;
+    const dateCreated = new Date().toLocaleDateString("en-IN");
+    const analyticsEntry = await prisma.analytics.upsert({
+      where: {
+        analytics_unique_key: {
+          shop: shopUrl.slice(8),
+          page: path,
+          createdAt: dateCreated,
+        },
+      },
+      update: {
+        clicked: { increment: clicked },
+      },
+      create: {
+        shop: shopUrl.slice(8),
+        page: path,
+        country: country,
+        device: device_type,
+        clicked: clicked,
+        os: platform,
+        createdAt: dateCreated,
+      },
+    });
+    return c.json({ analyticsEntry });
+  }
+  catch (e) {
+    console.error(e);
+    return c.json({ error: e }, 500);
+  }
+})
+
 
 export default proxyRouter;
