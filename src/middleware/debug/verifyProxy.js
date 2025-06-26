902d74afe62054a3efd8a8ccdd9429be2ca69f31
ctrlKshav <keshavm9978@gmail.com>
2025-06-26 20:33:35 +0530

    Fix Bug in Debug Proxy Middleware

--- src/middleware/debug/verifyProxy.js
+++ src/middleware/debug/verifyProxy.js
@@ -2,7 +2,7 @@
 const verifyProxy = async (c, next) => {
 
     const query = c.req.query();
-    if (!query.shop || !query.signature) {
+    if (!query.shop) {
         return c.json({ error: "Unauthorized call" });
     }
     c.req.user_shop = query.shop;
