42da3f5ab570861dbff58757d1306f4531dc2f89
ctrlKshav <keshavm9978@gmail.com>
2025-06-26 20:24:50 +0530

    Separate Router for graphQL

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,18 @@
 import { Hono } from 'hono'
 import { logger } from 'hono/logger'
 
-import sessionHandler from "./utils/sessionHandler.js";
 import setupCheck from "./utils/setupCheck.js";
-import shopify from "./utils/shopify.js";
 
 import gdprHandlers from "./controllers/gdpr.js";
 
 import csp from "./middleware/csp.js";
 import isInitialLoad from "./middleware/isInitialLoad.js";
 import verifyHmac from "./middleware/verifyHmac.js";
-import verifyRequest from "./middleware/verifyRequest.js";
 
 import proxyRouter from "./routes/app_proxy/index.js";
 import checkoutRouter from "./routes/checkout/index.js";
 import appRouter from "./routes/app_routes/index.js";
+import graphQLRouter from "./routes/graphql/index.js";
 
 import webhookHandler from "./webhooks/_index.js";
 
@@ -35,24 +33,7 @@ app.post(
   webhookHandler
 );
 
-app.post("/api/graphql", verifyRequest, async (c) => {
-  try {
-    const sessionId = await shopify.session.getCurrentId({
-      isOnline: true,
-      rawRequest: c.req,
-      rawResponse: c.res,
-    });
-    const session = await sessionHandler.loadSession(sessionId);
-    const response = await shopify.clients.graphqlProxy({
-      session,
-      rawBody: await c.req.parseBody(),
-    });
-    return c.json(response.body);
-  } catch (e) {
-    console.error(`---> An error occured at GraphQL Proxy`, e);
-    return c.json({ error: e }, 403);
-  }
-});
+app.route("/api/graphql", graphQLRouter);
 
 app.use(csp)
 
---
+++ src/routes/graphql/index.js
@@ -0,0 +1,29 @@
+﻿import shopify from "../../utils/shopify.js";
+import sessionHandler from "../../utils/sessionHandler.js";
+import { Hono } from "hono";
+import verifyRequest from "../../middleware/verifyRequest.js";
+
+const graphQLRouter = new Hono();
+
+graphQLRouter.post("/graphql", verifyRequest, graphQLHandler);
+
+const graphQLHandler = async (c) => {
+    try {
+        const sessionId = await shopify.session.getCurrentId({
+            isOnline: true,
+            rawRequest: c.req,
+            rawResponse: c.res,
+        });
+        const session = await sessionHandler.loadSession(sessionId);
+        const response = await shopify.clients.graphqlProxy({
+            session,
+            rawBody: await c.req.parseBody(),
+        });
+        return c.json(response.body);
+    } catch (e) {
+        console.error(`---> An error occured at GraphQL Proxy`, e);
+        return c.json({ error: e }, 403);
+    }
+}
+
+export default graphQLRouter;
